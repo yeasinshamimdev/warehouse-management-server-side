a86751654ac1b869153047966aa7ebec9a4d41ed
Shamim <yeasinshamim7@gmail.com>
2022-05-01 13:19:21 +0600

    mongodb added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,8 +9,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqnij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const sportsCollection = client.db("sports-gear-warehouse").collection("sports-items");
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = await sportsCollection.find(query).toArray();
+            res.send(cursor);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-    res.send('sports gear warehouse server');
+    res.send('sports gear warehouse server running');
 });
 
 app.listen(port, () => {
