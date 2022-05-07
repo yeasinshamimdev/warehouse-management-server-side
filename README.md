83cf55c05def1731dfd8801afde0fa85255cd6e0
Shamim <yeasinshamim7@gmail.com>
2022-05-08 00:01:30 +0600

    almost done

---
+++ README.md
@@ -0,0 +1,27 @@
+# Sports gear warehouse
+
+Live site Link: [https://sports-gear-warehouse.web.app/](https://sports-gear-warehouse.web.app/).
+
+Server site Link: [https://whispering-garden-12680.herokuapp.com](https://whispering-garden-12680.herokuapp.com)
+
+Stack Overflow Question: [stackoverflow question](https://stackoverflow.com/questions/72074941/i-want-deploy-my-react-js-site-in-firebase-but-i-found-this-error-how-can-i-fi)
+
+## Description:
+
+This is my sports gear warehouse server. Here Used nodejs expressjs for create server. I used Mongodb as database. I stored my all items in mongodb database. User can call with products api for see all items. But big things is invalid user cann't see the private data of user. Only valid user see the data of his/her.
+
+## Website's Features:
+
+* Here I used cors as middleware.
+* I used dotenv for secure my database user name and password.
+* I used express js for create server.
+* I used json web token for secure private api.
+* I used mongodb as my database.
+
+## Website's Functionality
+
+* In this server site, you can access products api.
+* You can see all the products by call them.
+* When you login client side, the server create a secret key.
+* Here you can see the customer review api.
+* When user will post any data the server will save them in database.
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -8,6 +9,23 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqnij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,11 +36,30 @@ async function run() {
         const sportsCollection = client.db("sports-gear-warehouse").collection("sports-items");
         const reviewCollection = client.db("sports-gear-warehouse").collection("reviews");
 
-        app.get('/products', async (req, res) => {
-            const query = {};
-            const cursor = sportsCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
+        // jwt token 
+        app.post('/login', async (req, res) => {
+            const email = req.body;
+
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ token })
+        })
+
+
+        app.get('/products', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+
+            if (email === decodedEmail) {
+                const query = {};
+                const cursor = sportsCollection.find(query);
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden access' });
+            }
         });
 
         // get review
